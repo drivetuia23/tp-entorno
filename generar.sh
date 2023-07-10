7eea1b0ff8945162674fc10d8e6798d6fe2c3c73
DaldiB <136038912+DaldiB@users.noreply.github.com>
2023-07-10 10:46:55 -0300

    Add files via upload
    Prueba 0.1 del script generar

---
+++ generar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+# Autor: Mariana Bettoni
+# Versión 0.1
+# La intención del script es descargar una imagen desde la web, se debe poder indicar POR argumento cuantas imagenes
+# generary se deben asignar nombres de archivos al azar de una lista de nombres de personas. 
+# Luego se generará un archivo comprimido con las imagenes y entregará la suma de verificación.
+
+

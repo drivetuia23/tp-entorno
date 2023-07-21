d4a552cd3d4324e76ba11d50a23b0fd991cba4c8
drivetuia23 <136250619+drivetuia23@users.noreply.github.com>
2023-07-21 19:33:40 -0300

    Modificacion de metodo de lectura de datos (nombres)
    Los datos se leen desde una url web que provee nombres de personas en un csv

--- generar.sh
+++ generar.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-
-
 # Verificar la cantidad de imágenes a generar (entre 1 y 300)
 cantidad_imagenes=$1
 
@@ -10,24 +8,27 @@ if [ -z "$cantidad_imagenes" ]; then
   exit 1
 fi
 
-if (( cantidad_imagenes < 1 || cantidad_imagenes > 300 )); then
-  echo "La cantidad de imágenes a generar debe estar entre 1 y 300."
-  exit 1
-fi
-
 # Crear directorio para las imágenes
 directorio="img"
-# mkdir -p "$directorio"
+mkdir -p "$directorio"
 
-# Nombres de archivo aleatorios
-nombres=("John" "Jane" "Mari" "Claudio" "javi" "soraya" "Natalia" "Alice" "Bob" "Eve" "Ana" "paula" "juan")  # acá entra la list con los 300 nombres
+# Nombres de personas desde servicio web .csv
+
+csv_file=0
+directorio_destino="/tp-entorno"
+
+# Descargar el archivo CSV con los nombres desde la Web
+wget -O "$directorio_destino/$csv_file" "https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv"
+
+# Pasar los nombres del archivo CSV a un array
+mapfile -t nombres < "$directorio_destino/$csv_file"
 
 # Descargar imágenes desde el servicio web
 for ((i=1; i<=cantidad_imagenes; i++))
 do
   # Nombre aleatorio de archivo
   nombre_archivo="${nombres[$RANDOM % ${#nombres[@]}]}"
-  
+    
   # Descargar imagen desde https://thispersondoesnotexist.com/
   wget -q -O "$directorio/$nombre_archivo" "https://thispersondoesnotexist.com/"
 
@@ -47,10 +48,3 @@ md5sum imagenes.tar.gz | awk '{print $1}' > verificacion.txt
 
 # Mostrar información
 echo "Imágenes generadas y comprimidas en imagenes.tar.gz"
-
-
-
-
-
-
-

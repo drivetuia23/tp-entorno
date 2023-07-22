a568094f7b76a25260d1f75b7d8a54992c57fcd5
drivetuia23 <136250619+drivetuia23@users.noreply.github.com>
2023-07-22 15:10:48 -0300

    Mejoramiento regex para contar nombres finalizados con "a"
    Se ajustó la expresión regular utilizada en la función de contar nombres terminados en 'a' para que considere correctamente el formato nombre,número de los archivos de entrada. Esto arregla un error donde no se contabilizaban correctamente todos los nombres terminados en 'a'."

--- comprimir.sh
+++ comprimir.sh
@@ -2,35 +2,33 @@
 
 directorio="img"
 directorio_procesadas="procesadas"
+directorio_salida="generados"
+
 if [ -d "$directorio" ] && [ -d "$directorio_procesadas" ]; then
-	# Directorio donde se encuentran las imágenes procesadas
-	directorio_procesadas="procesadas"
-
-	# Directorio donde se almacenarán los archivos generados
-	directorio_salida="generados"
-
-	# Crear directorio de salida si no existe
+	
+ 	# Crear directorio de salida si no existe
 	mkdir -p "$directorio_salida"
 
 	# Generar archivo con la lista de nombres de todas las imágenes
-	echo "Generando archivo con la lista de nombres de todas las imágenes..."
+	echo "Archivo con la lista de nombres de todas las imágenes: OK"
 	find "$directorio_procesadas" -type f -exec basename {} \; > "$directorio_salida/lista_nombres_imagenes.txt"
 
 	# Generar archivo con la lista de nombres válidos
-	echo "Generando archivo con la lista de nombres válidos..."
+	echo "Archivo con la lista de nombres válidos: OK"
 	grep -E '[A-Z][a-z]+' "$directorio_salida/lista_nombres_imagenes.txt" > "$directorio_salida/lista_nombres_validos.txt"
 
 	# Generar archivo con el total de personas cuyo nombre finaliza con la letra 'a'
-	echo "Generando archivo con el total de personas cuyo nombre finaliza con la letra 'a'..."
-	grep -E '[A-Z][a-z]+a&' "$directorio_salida/lista_nombres_validos.txt" | wc -l > "$directorio_salida/total_nombres_con_a.txt"
-
+	echo "Archivo con el total de nombres finalizados con 'a': OK"
+	grep -E '[A-Za-z]+a[^,]*$' "$directorio_salida/lista_nombres_validos.txt" | wc -l > "$directorio_salida/total_nombres_con_a.txt"
+	# grep modificado para que lea hasta la coma dentro del nombre de archivo (segun formato nombre,numero)
+  
 	# Crear archivo comprimido con todos los archivos generados y las imágenes
-	echo "Creando archivo comprimido..."
+	echo "Archivo comprimido: OK"
 	tar -czf archivos_generados.tar.gz "$directorio_salida" "$directorio_procesadas"
 
-	echo "Proceso completado. Se han generado los archivos solicitados y el archivo comprimido 'archivos_generados.tar.gz' que incluye todo el contenido."
+	echo "Proceso completado. Se generaron los archivos con info de las imágenes y comprimido 'archivos_generados.tar.gz' que incluye todo el contenido."
 	sleep 4
 else 
-	echo "No puede ejecutar este programa."
+	echo "No puede ejecutar este programa porque no existe el directorio de imágenes y/o el directorio de procesadas."
 	sleep 2
 fi
